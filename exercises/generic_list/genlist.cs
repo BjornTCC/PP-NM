21ed23ceb59afc8421defbadaef2b50355944a6c
Bjorn <asbjornteilmann@gmail.com>
2024-02-14 14:09:10 +0100

    remaing genlist ex

--- exercises/generic_list/genlist.cs
+++ exercises/generic_list/genlist.cs
@@ -1,15 +1,50 @@
+using System;
+using static System.Console;
+
 public class genlist<T>{
 
 public T[] data;
-public T this[int i] {get{return data[i];} set{data[i]=value;}}
-public int size => data.Length;
+public T this[int i] {get{return data[i];} set{data[i]=value;} }
+public int size = 0, capacity = 1;
 
-public genlist(){data = new T[0];}
+public genlist(){data = new T[capacity];}
 public void add (T item){
-	T[] newdata = new T[data.Length+1];
-	for(int i=0;i < data.Length;i++)newdata[i]=data[i];
-	newdata[data.Length] = item;
-	data = newdata;
+	if(size == capacity){
+		T[] newdata = new T[capacity*=2];
+		for(int i=0;i < size;i++)newdata[i]=data[i];
+		data = newdata;
+		}
+	data[size] = item;
+	size++;	
 	}
 
+public void remove(int i){
+	if (-1 < i & i < size ){
+		for(int j = i; j< size-1;j++)data[j] = data[j+1];
+		size--;
+		}
+	else {Error.WriteLine($"Error: Index {i} not in list of length {data.Length}");}
+	}
+}
+
+public class node<T>{
+	public T item;
+	public node<T> next;
+	public node(T item){this.item=item;}
+}
+
+public class clist<T>{
+        public node<T> first=null,current=null;
+        public void add(T item){
+                if(first==null){
+                        first=new node<T>(item);
+                        current=first;
+                }
+                else{
+                        current.next = new node<T>(item);
+                        current=current.next;
+                }
+        }
+        public void start(){ current=first; }
+        public void next(){ current=current.next; }
 }
--- exercises/generic_list/main.cs
+++ exercises/generic_list/main.cs
@@ -2,7 +2,7 @@ using System;
 using static System.Console;
 
 static class main{
-
+	
 public static int Main(){
         var list = new genlist<double[]>(); 
 	char[] delimiters = {' ','\t'};
@@ -19,6 +19,26 @@ public static int Main(){
 		foreach(var number in numbers)Write($"{number : 0.00e+00;-0.00e+00} ");
 		WriteLine();
         	}
+	//Test of add and remove
+	var RList = new genlist<double>();
+	RList.add(1.0);
+	RList.add(2.0);
+	RList.add(3.0);
+	RList.add(4.0);
+	for(int i = 0; i < RList.size; i++)Write($"{RList[i]},");
+	WriteLine();
+	RList.remove(1);
+	RList.remove(4);
+	for(int i = 0; i < RList.size; i++)Write($"{RList[i]},");
+	WriteLine();
+	//Test of clist
+	clist<int> a = new clist<int>();
+                a.add(1);
+                a.add(2);
+                a.add(3);
+                for( a.start(); a.current != null; a.next()){
+                        WriteLine(a.current.item);
+			}
 	return 0;
         }
 }
