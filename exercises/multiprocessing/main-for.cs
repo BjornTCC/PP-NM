075b8599c72c4c6fe39145e7a3ca04bed0c0ea8d
Bjorn <asbjornteilmann@gmail.com>
2024-02-21 11:31:14 +0100

    multiprocessing completed

--- exercises/multiprocessing/main-for.cs
+++ exercises/multiprocessing/main-for.cs
@@ -8,13 +8,9 @@ public class main{
                         if(words[0] == "-nterms")nterms = (int)double.Parse(words[1]);
 			}
 		System.Console.WriteLine($"Main: nterms = {nterms}");
-		double sum = 0;
-		System.Threading.Tasks.Parallel.For(1, nterms+1, delegate(int i){sum += 1.0/i;});
-		System.Console.WriteLine($"Main: Simple Parallel.For: sum = {sum}");
-		//with better splitting
 		var total = new System.Threading.ThreadLocal<double>( ()=>0, trackAllValues:true);
 		System.Threading.Tasks.Parallel.For( 1, nterms+1, (int i)=>total.Value+=1.0/i );
 		double totalsum=total.Values.Sum();
-		System.Console.WriteLine($"Main: Better Parallel.For: sum = {totalsum}");
+		System.Console.WriteLine($"Main: sum = {totalsum}");
 	}//Main
 }//main
