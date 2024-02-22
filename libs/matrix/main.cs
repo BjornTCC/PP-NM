b9b5a23644a2f1242f8f6b8cd527113263a24b6b
Bjorn <asbjornteilmann@gmail.com>
2024-02-22 17:43:50 +0100

    matrix class made

---
+++ libs/matrix/main.cs
@@ -0,0 +1,15 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+static class main{
+	static void Main(){
+		matrix M = new matrix("1 0 1; 0 1 0 ; 0 0 1");
+		matrix b = matrix.id(3);
+		b.print();
+		matrix B = M*b; 
+		M.print();
+		B.print();
+	}
+}
+
