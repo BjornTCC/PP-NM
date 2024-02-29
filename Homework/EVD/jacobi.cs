1658f6691b591e5af68d0b665c8e811e82fd3503
Bjorn <asbjornteilmann@gmail.com>
2024-02-29 15:12:12 +0100

    Created optimized jacobi algorithm

--- Homework/EVD/jacobi.cs
+++ Homework/EVD/jacobi.cs
@@ -41,4 +41,34 @@ public static class jacobi{
 		for(int i = 0; i < w.size; i++)w[i] = D[i,i]; //collect the eigenvalues as the diagonal elements of D
 		return (w, V);
 	}//cyclic
+	
+	public static (vector, matrix) cyclic_opt(matrix A){
+		matrix D = A.copy(), V = matrix.id(A.size1);
+                vector w = new vector(D.size1);
+                bool changed;
+                do{
+                        changed = false; //assume succesful diagonalization until proven otherwise
+                        for(int p = 0; p < D.size1-1; p++) //loop over upper triangle part of matrix to do rotation
+                        for(int q = p+1; q < D.size2; q++){
+                                double theta = 0.5*Atan2(2*D[p,q], D[q,q] - D[p,p]); //find theta
+                                double c = Cos(theta), s = Sin(theta);
+                                double new_Dpp = c*c*D[p,p] - 2*s*c*D[p,q] + s*s*D[q,q]; //find new diagonal elements
+                                double new_Dqq = c*c*D[q,q] + 2*s*c*D[p,q] + s*s*D[p,p];
+                                if(new_Dpp != D[p,p] || new_Dqq != D[q,q]){
+					changed = true;
+					timesJ(V, p, q, theta); 			//V <- VJ
+					D[p,q] = s*c*(D[p,p] - D[q,q]) + (c*c - s*s)*D[p,q]; //update cross point
+					D[p,p] = new_Dpp; D[q,q] = new_Dqq; 		//update diagonal
+					for(int i = 0; i < D.size2; i++){
+					if(i!=q && i!=p){
+						int Mqi = Max(q,i), mqi = Min(q,i), Mpi = Max(p,i), mpi = Min(p,i); 
+						double Dpi = D[mpi, Mpi], Dqi = D[mqi,Mqi]; //set old data 
+						D[mpi,Mpi] = c*Dpi - s*Dqi;			//update data
+						D[mqi,Mqi] = s*Dpi + c*Dqi;}}
+					}
+			}
+		}while(changed);
+		for(int i = 0; i < w.size; i++)w[i] = D[i,i]; //collect the eigenvalues as the diagonal elements of D
+                return (w, V);
+	}//cyclic_opt
 }//jacobi
--- Homework/EVD/main.cs
+++ Homework/EVD/main.cs
@@ -2,7 +2,7 @@ using static System.Console;
 
 public class main{
 	public static void Main(){
-		int n = 6;
+		int n = 3;
 		matrix A = new matrix(n);
 		System.Random random = new System.Random();
 		for(int i = 0; i < n; i++)
@@ -12,9 +12,12 @@ public class main{
 		}
 		A.print("A =");
 		(vector w, matrix V) = jacobi.cyclic(A);
+		(vector w_opt, matrix V_opt) = jacobi.cyclic_opt(A);
+		w.print("EigenValues: ");
+		w_opt.print("Eigenvalues using opt:");
+		V.print("Eigenvectors = ");
+		V_opt.print("Eigenvectors using opt = ");
 		matrix D = matrix.diag(w);
-		D.print("D approx: ");
-		V.print("eigenvectors = ");
 		matrix VT = V.transpose();
 		matrix VTAV = VT*A*V;
 		matrix VDVT = V*D*VT;
