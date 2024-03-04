abe1aef1770182d4fe34cdcbd4de8bc0e6eb1cb3
Bjorn <asbjornteilmann@gmail.com>
2024-03-04 16:06:43 +0100

    -

--- Homework/Splines/mainq.cs
+++ Homework/Splines/mainq.cs
@@ -6,20 +6,20 @@ public class main{
 	public static int Main(string[] args){
 	//Config extra output file
 		string outfile=null;
-	foreach(var arg in args){
-		string[] words = arg.Split(':');
-		if(words[0]=="-output")outfile=words[1];
+		foreach(var arg in args){
+			string[] words = arg.Split(':');
+			if(words[0]=="-output")outfile=words[1];
 		}
-	if(outfile==null){
-		System.Console.Error.WriteLine("wrong argument");
-		return 1;
+		if(outfile==null){
+			System.Console.Error.WriteLine("wrong argument");
+			return 1;
 		}
-	var outstream = new System.IO.StreamWriter(outfile,append:false);
+		var outstream = new System.IO.StreamWriter(outfile,append:false);
 	//start of actual program
 		int N = 5, M = 100;
                 vector x = new vector(N), y1 = new vector(N), y2 = new vector(N), y3 = new vector(N), z = new double[M];
                 for(int i = 0; i < N; i++){
-                        x[i] = i;
+                        x[i] = i+1;
                         y1[i] = 1;
 			y2[i] = x[i];
 			y3[i] = x[i]*x[i];
@@ -33,15 +33,25 @@ public class main{
                 vector c1 = new vector("0 0 0 0");
 		vector c2 = new vector("0 0 0 0");
 		vector c3 = new vector("1 1 1 1");//analytic c's
-		if(c1.approx(qs1.c) && c2.approx(qs2.c) && c3.approx(qs3.c)){
+		vector b1 = new vector("0 0 0 0");
+                vector b2 = new vector("1 1 1 1");
+                vector b3 = new vector("2 4 6 8");//analytic b's
+		if(c1.approx(qs1.c) && c2.approx(qs2.c) && c3.approx(qs3.c) 
+				&& b1.approx(qs1.b) && b2.approx(qs2.b) && b3.approx(qs3.b)){
 			WriteLine("Comparison test with analytical values: Success");} 
 			else {WriteLine("Comparison test with analytical values: Failure");}
-		c1.print("c1:");
-		qs1.c.print("spline1:");
-		c2.print("c2:");
-                qs2.c.print("spline2:");
-		c3.print("c3:");
-                qs3.c.print("spline3:");
+		qs1.c.print("c1 spline:");
+		qs1.b.print("b1 spline:");
+		c1.print("c1 manual:");
+                b1.print("b1 manual:");
+		qs2.c.print("c2 spline:");
+                qs2.b.print("b2 spline:");
+                c2.print("c2 manual:");
+                b2.print("b2 manual:");
+		qs3.c.print("c3 spline:");
+                qs3.b.print("b3 spline:");
+                c3.print("c3 manual:");
+                b3.print("b3 manual:");
         	outstream.Close();
 		return 0;
 	}//Main
