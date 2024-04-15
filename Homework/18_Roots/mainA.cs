fcbfa7732114a8141be694468044bb301ab84a97
Bjorn <asbjornteilmann@gmail.com>
2024-04-15 17:15:47 +0200

    Changed root class -> namespace. Removed unused simplex class

--- Homework/18_Roots/mainA.cs
+++ Homework/18_Roots/mainA.cs
@@ -4,55 +4,63 @@ using static System.Math;
 
 public class main{
 	public static int Main(){
-		int f_eval = 0;
 		//Three simple functions
-		Func<vector,vector> f1 = delegate(vector v){vector w = new vector(1); f_eval++;
+		Func<vector,vector> f1 = delegate(vector v){vector w = new vector(1); 
 		       					w[0] = v[0]*Exp(-v[0]*v[0]); return w;};
 		matrix xs = new matrix("-0.5 0 0.5");
                 WriteLine("f = x*e(-x^2). v0 = 0");
                 WriteLine("");
                 for(int i=0;i<3;i++){
                         vector x = xs[i];
-                        (vector xmin,vector fmin,int steps) = root.newton(f1,x);
+                        root.newton root = new root.newton(f1,x);
+			vector xmin = root.x;
+			vector fmin = root.f;
+			int steps = root.steps;
+			int f_eval = root.f_eval;
                         WriteLine($"Numerical results, v0 = {x[0]}:");
                         WriteLine($"v0 = {xmin[0]}. f(v0) = {fmin[0]}");
 			WriteLine($"Steps taken: {steps}. Function evals: {f_eval}");
                         WriteLine("");
-			f_eval = 0;
                 }
 
-		Func<vector,vector> f2 = delegate(vector v){vector w = new vector(2); f_eval++;
+		Func<vector,vector> f2 = delegate(vector v){vector w = new vector(2); 
 							w[0] = 1+v[1]*v[0]; w[1] = 1-v[0]*v[0]; return w;};
                 matrix xs2 = new matrix("-0.5 0.001 0.001;0 -0.001 0.5");
                 WriteLine("(x,y) -> (1+xy, 1-x^2). v0 = (1,-1) & (-1,1)");
                 WriteLine("");
                 for(int i=0;i<3;i++){
                         vector x = xs2[i];
-                        (vector xmin,vector fmin,int steps) = root.newton(f2,x);
+                        root.newton root = new root.newton(f2,x);
+                        vector xmin = root.x;
+                        vector fmin = root.f;
+                        int steps = root.steps;
+                        int f_eval = root.f_eval;
                         WriteLine($"Numerical results, v0 = ({x[0]}, {x[1]}):");
                         WriteLine($"v0 = ({xmin[0]}, {xmin[1]}). f(v0) = ({fmin[0]}, {fmin[1]})");
                         WriteLine($"Steps taken: {steps}. Function evals: {f_eval}");
                         WriteLine("");
-			f_eval = 0;
 		}
 
-		Func<vector,vector> f3 = delegate(vector v){vector w = new vector(2); f_eval++;
+		Func<vector,vector> f3 = delegate(vector v){vector w = new vector(2); 
 		       					w[0] = Pow(v[0]-1,2); w[1] = Log(v[0]); return w;};
 		matrix xs3 = new matrix("0.5 1.2 2");
                 WriteLine("f = ((x-1)^2, ln(x)). x0 = 1");
                 WriteLine("");
                 for(int i=0;i<3;i++){
                         vector x = xs3[i];
-                        (vector xmin,vector fmin,int steps) = root.newton(f3,x);
+                        root.newton root = new root.newton(f3,x);
+                        vector xmin = root.x;
+                        vector fmin = root.f;
+                        int steps = root.steps;
+                        int f_eval = root.f_eval;
                         WriteLine($"Numerical results, x0 = {x[0]}:");
                         WriteLine($"x0 = {xmin[0]}. f(x0) = ({fmin[0]}, {fmin[1]}).");
                         WriteLine($"Steps taken: {steps}. Function evals: {f_eval}");
                         WriteLine("");
-			f_eval = 0;
                 }
 		//Rosenbrock function part
-		Func<vector,double> Rf = delegate(vector v){f_eval++;return Pow(1-v[0],2)+100*Pow(v[1] - v[0]*v[0],2);};
-		Func<vector,vector> dRf = delegate(vector v){vector w = new vector(2); f_eval++;
+		Func<vector,double> Rf = delegate(vector v){return Pow(1-v[0],2)+100*Pow(v[1] - v[0]*v[0],2);};
+		Func<vector,vector> dRf = delegate(vector v){vector w = new vector(2);
 							w[0] = -2*(1-v[0]) - 400*v[0]*(v[1]-v[0]*v[0]);
 							w[1] = 200*(v[1]-v[0]*v[0]); return w;};
 		matrix x0s_R = new matrix("2 0 0; 0 0 2");
@@ -60,18 +68,21 @@ public class main{
 		WriteLine("");
 		for(int i=0;i<3;i++){
 			vector x0 = x0s_R[i];
-                        (vector xmin,vector dfmin,int steps) = root.newton(dRf,x0);
+                        root.newton root = new root.newton(dRf,x0);
+                        vector xmin = root.x;
+                        vector dfmin = root.f;
+                        int steps = root.steps;
+                        int f_eval = root.f_eval;
 			double fmin = Rf(xmin);
 			WriteLine($"Numerical results, v0 = ({x0[0]}, {x0[1]}):");
 			WriteLine($"vmin = ({xmin[0]}, {xmin[1]}). f(vmin) = {fmin}. df(vmin) = ({dfmin[0]}, {dfmin[1]})");
                         WriteLine($"Steps taken: {steps}. Function evals: {f_eval}");
 			WriteLine("");
-			f_eval = 0;	
 		}
 
 		//Himmelblau function part
-                Func<vector,double> Hf = delegate(vector v){f_eval++;return Pow(v[0]*v[0]+v[1]-11,2)+Pow(v[0] + v[1]*v[1]-7,2);};
-                Func<vector,vector> dHf = delegate(vector v){vector w = new vector(2); f_eval++;
+                Func<vector,double> Hf = delegate(vector v){;return Pow(v[0]*v[0]+v[1]-11,2)+Pow(v[0] + v[1]*v[1]-7,2);};
+                Func<vector,vector> dHf = delegate(vector v){vector w = new vector(2);
                                                         w[0] = 4*v[0]*(v[0]*v[0]+v[1]-11)+2*(v[0]+v[1]*v[1]-7);
                                                         w[1] = 2*(v[0]*v[0]+v[1]-11) + 2*v[1]*(v[0]+v[1]*v[1]-7); 
 							return w;};
@@ -81,7 +92,11 @@ public class main{
                 WriteLine("");
                 for(int i=0;i<4;i++){
                         vector x0 = x0s_H[i];
-                        (vector xmin,vector dfmin,int steps) = root.newton(dHf,x0);
+                        root.newton root = new root.newton(dHf,x0);
+                        vector xmin = root.x;
+                        vector dfmin = root.f;
+                        int steps = root.steps;
+                        int f_eval = root.f_eval;
                         double fmin = Hf(xmin);
                         WriteLine($"Numerical results, v0 = ({x0[0]}, {x0[1]}):");
                         WriteLine($"vmin = ({xmin[0]}, {xmin[1]}). f(vmin) = {fmin}. df(vmin) = ({dfmin[0]}, {dfmin[1]})");
--- Homework/18_Roots/root.cs
+++ Homework/18_Roots/root.cs
@@ -2,67 +2,88 @@ using System;
 using static System.Console;
 using static System.Math;
 
-public class root{
-
-	static matrix jacobian(
-			Func<vector,vector> f, 
-			vector x,
-			vector f0 = null
-			){ 				//compute the jacobian of a function f
-		if(f0==null)f0 = f(x);
-		int m = x.size, n = f0.size;
-		matrix jac = new matrix(n,m);
-		double eps = Pow(2,-26), dx = 1;
-		vector x2 = x.copy(), df = new vector(n);
-		for(int i=0;i<m;i++){
-			dx = eps*Max(1,Abs(x2[i]));
-			x2[i] += dx;
-			df = f(x2) - f0;
-			jac[i] = df/dx;
-			x2[i] = x[i];}
-		return jac;
-	}//jacobian
+namespace root{
 	
-	public static (vector,vector,int) newton(
-			Func<vector,vector> f, 
+	public class newton{
+		public static readonly double ε = Pow(2,-26), λmin = Pow(2,-10);
+		
+                public readonly Func<vector,vector> F;
+		public vector x, f;
+		public int steps, f_eval;
+		public bool status;
+		
+		//constructor
+		public newton(
+			Func<vector,vector> func, 
 			vector x0, 
-			double eps = 1e-3,
-			int max_steps = (int)1e6){
-		double lambda = 1; int steps = 0;
-		vector x = x0.copy(), Dx = new vector(x0.size), f0 = f(x0), f1 = f0;
-                int m = x.size, n = f0.size;
-		do{
-		steps++;
-		matrix J = jacobian(f,x,f0);
-		Dx = QRGS.solve(J, -f0);
-		lambda = 1;
-		f1 = f(x + Dx);
-		while(f1.norm() > (1-lambda/2)*f0.norm() && 1/1024 < lambda){
-			lambda/=2;
-			f1 = f(x+lambda*Dx);
+			int max_steps = 9999,
+			double acc = 1e-4)
+			{
+			F = func; f_eval = 0;
+			double lambda = 1; 
+			steps = 0; f_eval = 1;
+			x = x0.copy(); vector Dx = new vector(x0.size), f0 = F(x0), f1 = f0;
+			status = true;
+			do{
+			steps++;
+			matrix J = jacobian(x,f0);
+			Dx = QRGS.solve(J, -f0);
+			lambda = 1;
+			f1 = F(x + Dx); f_eval++;
+			while(f1.norm() > (1-lambda/2)*f0.norm() && λmin < lambda){
+				lambda/=2;
+				f1 = F(x+lambda*Dx); f_eval++;
+				}
+			x+=lambda*Dx;
+			f0 = f1;
+			}while(f0.norm() >= acc && Dx.norm() >= ε*x.norm() && steps < max_steps);
+			if(steps >= max_steps)status = false;
+			f=f0;
 			}
-		x+=lambda*Dx;
-		f0 = f1;
-		}while(f0.norm() >= eps && Dx.norm() >= Pow(2,-26)*x.norm() && steps < max_steps);
-		if(steps >= max_steps)Error.WriteLine($"newton: Maximum step of {max_steps} reached");
-		return (x,f0,steps);
+		
+		matrix jacobian(
+                        vector x,
+                        vector f0 = null
+                        ){                              //compute the jacobian of a function f
+                	if(f0==null){f0 = F(x);f_eval++;}
+                	int m = x.size, n = f0.size;
+                	matrix jac = new matrix(n,m);
+                	vector x2 = x.copy(), df = new vector(n);
+                	for(int i=0;i<m;i++){
+                        	double dx = ε*Max(1,Abs(x2[i]));
+                      		x2[i] += dx;
+                        	df = F(x2) - f0; 
+				f_eval++;
+                        	jac[i] = df/dx;
+                        	x2[i] = x[i];}
+                	return jac;
+		}//jacobian	
 	}//newton
 	
-	public static (vector,vector,int) newton_quad_int(
-			Func<vector,vector> f, 
+	public class newton_quad_int{
+		public readonly Func<vector,vector> F;
+                public static readonly double ε = Pow(2,-26), λmin = Pow(2,-10);
+
+                public vector x, f;
+                public int steps, f_eval;
+                public bool status;
+		
+		public newton_quad_int(
+			Func<vector,vector> func, 
 			vector x0, 
-			double eps = 1e-3,
-			int max_steps = (int)1e6){
-		double lambda = 1; int steps = 0, _steps = 0;
-	        vector x = x0.copy(), Dx = new vector(x0.size), f0 = f(x0), f1 = f0;
-	        int m = x.size, n = f0.size;
+			double acc = 1e-4,
+			int max_steps = 9999){
+		F = func; f_eval = 1; status = true;
+		double lambda = 1; 
+		steps = 0; int _steps = 0;
+	        x = x0.copy(); vector Dx = new vector(x0.size), f0 = F(x0), f1 = f0;
 		double a = 0, b = 0, c = 0;
 		do{
                 steps++;
-		matrix J = jacobian(f,x,f0);
+		matrix J = jacobian(x,f0);
                 Dx = QRGS.solve(J, -f0);
 		lambda = 1;
-	       	f1 = f(x + Dx);	
+	       	f1 = F(x + Dx);	f_eval++;
 		_steps = 0;
 		c = 0.5*f0.dot(f0);                             //compute quad. interpolation
                 b = f0.dot(J*Dx);
@@ -70,13 +91,32 @@ public class root{
 				a = (f1.dot(f1)-c)/(lambda*lambda) - b/lambda;
 				if(0.1<-b/(2*a) && -b/(2*a)<=1)lambda = -b/(2*a);	//we wish to have lambda in (0,1]
 				else lambda/=2;
-				f1 = f(x+lambda*Dx);
+				f1 = F(x+lambda*Dx); f_eval++;
 				_steps++;
 				}
 		x+=lambda*Dx;
 		f0=f1;
-                }while(f0.norm() >= eps && Dx.norm() >= Pow(2,-26)*x.norm() && steps <= max_steps);
-                if(steps >= max_steps)Error.WriteLine($"newton_quad_int: Maximum step of {max_steps} reached");
-                return (x,f0,steps);
+                }while(f0.norm() >= acc && Dx.norm() >= ε*x.norm() && steps <= max_steps);
+                if(steps >= max_steps)status = false;
+                f = f0;
+		}
+
+		matrix jacobian(
+                        vector x,
+                        vector f0 = null
+                        ){                              //compute the jacobian of a function f
+                        if(f0==null){f0 = F(x);f_eval++;}
+                        int m = x.size, n = f0.size;
+                        matrix jac = new matrix(n,m);
+                        vector x2 = x.copy(), df = new vector(n);
+                        for(int i=0;i<m;i++){
+                                double dx = ε*Max(1,Abs(x2[i]));
+                                x2[i] += dx;
+                                df = F(x2) - f0;
+                                f_eval++;
+                                jac[i] = df/dx;
+                                x2[i] = x[i];}
+                        return jac;
+                }//jacobian
 	}//newton_quad_int
 }//root
