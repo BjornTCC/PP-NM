d52c5668d0889f5c53d1e5e8110df953462b7aab
Bjorn <asbjornteilmann@gmail.com>
2024-04-18 18:51:44 +0200

    Fixed two-sided jacobi algorithm. Generalized to matrix of any dimension

--- testing/SVD.cs
+++ testing/SVD.cs
@@ -6,21 +6,24 @@ public static class main{
 	public class SVD{
 	
 	public int steps,dim1,dim2;
-	public matrix A,U,D,V;
+	public matrix A,U,D,V,_V;
 
 	//constructor
 	
 	public SVD(matrix B, string method = "one-sided", double acc = System.Double.NaN, int max_steps = 9999){
-		if(method == "one-sided") SVD_one_sided(B,acc,max_steps);
-		else if(method == "two-sided") SVD_two_sided(B,acc,max_steps);
+		if(B.size2 > B.size1) A = B.transpose();
+		else A = B;
+		dim1 = A.size1; dim2 = A.size2;
+		if(method == "one-sided") SVD_one_sided(acc,max_steps);
+		else if(method == "two-sided") SVD_two_sided(acc,max_steps);
 		else throw new System.ArgumentException("SVD: Invalid method: "+method);
+                if(B.size2 > B.size1){A = A.transpose(); _V = V.copy(); V = U; U = _V;}
 	}
 
-	public void SVD_one_sided(matrix B,double acc, int max_steps){
+	public void SVD_one_sided(double acc, int max_steps){
 		if(System.Double.IsNaN(acc)) acc = Pow(2,-52);
-		steps = 0; dim1 = B.size1; dim2 = B.size2;
-		A=B; matrix A_new = A.copy();
-		D = matrix.id(dim1); V = matrix.id(dim2);
+		steps = 0; matrix A_new = A.copy();
+		D = matrix.id(dim2); V = matrix.id(dim2);
 		bool changed;
 		do{
 			changed = false;			// Assume success
@@ -38,11 +41,12 @@ public static class main{
 		U = A_new.copy(); for(int i=0;i<dim2;i++)U[i]/=D[i,i];	
 	}
 	
-	public void SVD_two_sided(matrix B,double acc, int max_steps){
+	public void SVD_two_sided(double acc, int max_steps){
                 if(System.Double.IsNaN(acc)) acc = Pow(2,-52);
-                steps = 0; dim1 = B.size1; dim2 = B.size2;
-                A=B; matrix A_new = A.copy();
-                D = new matrix(dim2); V = matrix.id(dim2); U = matrix.id(dim1);
+                steps = 0; matrix A_new = new matrix(dim2), Q = new matrix(dim1,dim2); 
+		if(dim1==dim2) A_new = A.copy();
+		else{var QRD = QRGS.decomp(A); A_new = QRD.Item2; Q = QRD.Item1;}
+                D = new matrix(dim2); V = matrix.id(dim2); U = matrix.id(dim2);
                 bool changed;
                 do{
                         changed = false;                        // Assume success
@@ -53,18 +57,17 @@ public static class main{
 				Jtimes(A_new,p,q,-θG);
                                 double θJ = 0.5*Atan2(A_new[p,q]+A_new[q,p], A_new[q,q] - A_new[p,p]);
                                 timesJ(A_new,p,q,θJ);
-				Jtimes(A_new,p,q,-θJ);
-				A_new.print(file:System.Console.Error);
+				Jtimes(A_new,p,q,-θJ);			
 				timesJ(U,p,q,θG);
 				timesJ(U,p,q,θJ);
                                 timesJ(V,p,q,θJ);
                                 double Dp = A_new[p,p], Dq = A_new[q,q];
-				if(Dp<0){Dp*=-1;U[p]*=-1;}
-                                if(Dq<0){Dq*=-1;U[q]*=-1;}
                                 if(Abs(D[p,p]-Dp)>acc || Abs(D[q,q]-Dq)>acc)changed = true;     // check convergence
                                 D[p,p] = Dp; D[q,q] = Dq;
                         }
                 }while(changed && steps < max_steps);
+		for(int p = 0;p<dim2;p++)if(D[p,p]<0){D[p,p]*=-1;U[p]*=-1;}
+		if(dim1!=dim2) U = Q*U;
         }
 
 	static void timesJ(matrix A, int p, int q, double θ){
@@ -83,12 +86,12 @@ public static class main{
                         A[p,i] = c*Api + s*Aqi;
                         A[q,i] = c*Aqi - s*Api;
                         }
-	}//Jtimes
+	 }//Jtimes
 
 	}//SVD
 
 	public static int Main(){
-		int n = 5, m = 5;
+		int n = 3, m = 5;
                 //Create a random matrix
                 matrix A = new matrix(n,m);
                 System.Random random = new System.Random();
@@ -103,10 +106,9 @@ public static class main{
                 matrix D = A_SVD.D, U = A_SVD.U, V = A_SVD.V;
 		D.print("D=");
                 U.print("U="); matrix UTU = U.transpose()*U;	
-		V.print("V="); matrix VTV = V.transpose()*V, VVT = V*V.transpose();
+		V.print("V="); matrix VTV = V.transpose()*V;
 		UTU.print("UT*U");
                 VTV.print("VT*V");
-                VVT.print("V*VT");
 
                 matrix res = U*D*V.transpose();
                 res.print("U*D*VT=");
